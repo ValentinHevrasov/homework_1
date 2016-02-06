a07100a84a51b49e790e461657e009cefb7741b8
ValArk <valentin-glava@mail.ru>
2016-02-07 00:06:21 +0200

    Сравнение точки X и Y через ARGV

--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
-1
+xt, yt, xp, yp = ARGV
+
+if xt==xp && yt==yp
+  puts 'Точка найдена!'
+elsif xt!=xp || yt!=yp
+  puts 'Близко, но нет'
+    if xt==xp && yt!=yp
+      puts 'х координата верна, y нет'
+    elsif xt!=xp && yt==yp
+      puts 'y координата верна, x нет'
+    end
+end
